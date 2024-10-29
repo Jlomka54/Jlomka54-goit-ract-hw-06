523546e03e34594a85f433883fcb0b0acc40818b
Bohdan Haponiuk <jlomka54@gmail.com>
2024-10-29 18:54:52 +0400

    add dispatch for contact add main.js persistGate

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,15 +1,17 @@
+import { useDispatch } from "react-redux";
 import { deleteContact } from "../../redux/contactsSlice";
 import css from "./Contact.module.css";
 
 const Contact = (props) => {
   const { name, number, id } = props;
+  const dispatch = useDispatch();
   return (
     <li className={css.contactItem} key={id}>
       <div className={css.box}>
         <p>{name}</p>
         <p>{number}</p>
       </div>
-      <button type="button" onClick={() => deleteContact(id)}>
+      <button type="button" onClick={() => dispatch(deleteContact(id))}>
         Delete
       </button>
     </li>
--- src/redux/store.js
+++ src/redux/store.js
@@ -37,4 +37,4 @@ export const store = configureStore({
     }),
 });
 
-export const persist = persistStore(store);
+export const persistor = persistStore(store);
